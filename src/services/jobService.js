2b816b47d61ca6e35587c322f6717ca621544529
esaalhashimi <esahh222@gmail.com>
2026-01-06 15:29:16 +0300

    show job details it is work but not completed

--- src/services/jobService.js
+++ src/services/jobService.js
@@ -1,23 +1,40 @@
-import axios from 'axios'
-import * as tokenService from './tokenService'
+import axios from "axios";
+import * as tokenService from "./tokenService";
 
-const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/jobs`
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/jobs`;
 
 const index = async () => {
+  try {
     const res = await axios.get(BASE_URL, {
-        headers: { Authorization: `Bearer ${tokenService.getToken()}` }
-    })
-    return res.data
-}
+      headers: { Authorization: `Bearer ${tokenService.getToken()}` },
+    });
+    return res.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 const create = async (formData) => {
+  try {
     const res = await axios.post(BASE_URL, formData, {
-        headers: { Authorization: `Bearer ${tokenService.getToken()}` }
-    })
-    return res.data
-}
+      headers: { Authorization: `Bearer ${tokenService.getToken()}` },
+    });
+    return res.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-export {
-    index,
-    create
-}
+const show = async (id) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/${id}`, {
+      headers: { Authorization: `Bearer ${tokenService.getToken()}` },
+    });
+
+    return response.data.job;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export { index, create, show };
